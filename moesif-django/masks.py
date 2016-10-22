ebc165badc2adb8ca01c891666f2f6bd9491ccfe
xinghengwang <xing@usetrove.com>
2016-10-22 14:12:01 -0700

    added mask body and mask headers. (still need to write all the tests)

---
+++ moesif-django/masks.py
@@ -0,0 +1,38 @@
+
+
+def mask_headers(headers, masks):
+    """
+    filters any headers (a dictionary) with key that matches those in the
+    masks (a list)
+    """
+    if headers is None:
+        return headers
+    if masks is None:
+        return headers
+
+    for member in masks:
+        headers.pop(member, None)
+
+    return headers
+
+
+def mask_body(body, masks):
+    """
+    recursively removes any element from body (dictionary or lists) that
+    have key matches masks.
+    """
+    if body is None:
+        return body
+    if masks is None:
+        return body
+
+    if type(body) == list:
+        return [mask_body(element, masks) for element in body]
+
+    if type(body) == dict:
+        for mask in masks:
+            body.pop(mask, None)
+        for key in body:
+            body[key] = mask_body(body[key], masks)
+
+    return body
--- moesif-django/middleware.py
+++ moesif-django/middleware.py
@@ -9,6 +9,7 @@ from moesifapi.moesif_api_client import *
 from moesifapi.models import *
 from django.http import HttpRequest, HttpResponse
 from .http_response_catcher import HttpResponseCatcher
+from .masks import *
 
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
@@ -18,13 +19,12 @@ def get_client_ip(request):
         ip = request.META.get('REMOTE_ADDR')
     return ip
 
-
 def moesif_middleware(get_response):
     # One-time configuration and initialization.
     middleware_settings = settings.MOESIF_MIDDLEWARE
     client = MoesifAPIClient(middleware_settings.get('APPLICATION_ID'))
     # below comment for setting moesif base_uri to a test server.
-    # Configuration.BASE_URI = 'http://192.168.0.5:8000/_moesif/api'
+    Configuration.BASE_URI = 'http://192.168.0.5:8000/_moesif/api'
     api_version = middleware_settings.get('API_VERSION')
     api_client = client.api
     response_catcher = HttpResponseCatcher()
@@ -35,31 +35,39 @@ def moesif_middleware(get_response):
         # the view (and later middleware) are called.
 
         req_time = timezone.now()
-        req_body = None
-        if request.body:
-            print("about to serialize body" + request.body)
-            req_body = APIHelper.json_deserialize(request.body)
-        ip_address = get_client_ip(request)
 
         response = get_response(request)
         # Code to be executed for each request/response after
         # the view is called.
 
 
-        uri = request.scheme + "://" + request.get_host() + request.get_full_path()
+        req_headers = {}
+        if request.META is not None:
+            req_headers = request.META.copy()
+            req_headers = mask_headers)req_headers, middleware_settings.get('REQUEST_HEADER_MASKS')
 
+        req_body = None
+        if request.body is not None:
+            # print("about to serialize request body" + request.body)
+            req_body = APIHelper.json_deserialize(request.body)
+            req_body = mask_body(req_body, middleware_settings.get('REQUEST_BODY_MASKS'))
+
+        ip_address = get_client_ip(request)
+        uri = request.scheme + "://" + request.get_host() + request.get_full_path()
 
         def mapper(key):
             return response[key]
         # a little hacky, using _headers, which is intended as a private variable.
         rsp_headers = {k: mapper(k) for k, v in response._headers.items()};
+        rsp_headers = mask_headers(rsp_headers, middleware_settings.get('RESPONSE_HEADER_MASKS'))
 
         print("response headers:")
         print(rsp_headers);
 
-        rsp_body = APIHelper.json_deserialize(response.content);
-        if not response.content:
-            rsp_body = {}
+        rsp_body = {}
+        if response.content is not None:
+            rsp_body = APIHelper.json_deserialize(response.content)
+            rsp_body = mask_body(rsp_body, middleware_settings.get('RESPONSE_BODY_MASKS'))
 
         rsp_time = timezone.now()
 
@@ -68,7 +76,7 @@ def moesif_middleware(get_response):
                                       verb=request.method,
                                       api_version=api_version,
                                       ip_address=ip_address,
-                                      headers=request.META,
+                                      headers=req_headers,
                                       body=req_body)
 
         event_rsp = EventResponseModel(time=req_time.isoformat(),
@@ -76,10 +84,19 @@ def moesif_middleware(get_response):
                                        headers=rsp_headers,
                                        body=rsp_body)
 
+        username = None
+        if request.user:
+            if request.user.is_authenticated():
+                username = request.user.username
+
+        session_token = None
+        if request.session:
+            session_token = request.session.session_key
+
         event_model = EventModel(request=event_req,
                                  response=event_rsp,
-                                 user_id="my_user_id",
-                                 session_token="23jdf0owekfmcn4u3qypxg09w4d8ayrcdx8nu2ng]s98y18cx98q3yhwmnhcfx43f")
+                                 user_id=username,
+                                 session_token=session_token)
 
         def sending_event():
             print("sending event to moesif")
