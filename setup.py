5f5df642311e291eef330d7fc261ff7501aeaff5
dgilling <derric@moesif.com>
2017-10-11 12:38:04 -0700

    Bump Version to 1.2.0. Add *.rst file.

--- setup.py
+++ setup.py
@@ -17,8 +17,10 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+long_description = ''
+if path.exists('README.rst'):
+    with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+        long_description = f.read()
 
 setup(
     name='moesifdjango',
@@ -26,7 +28,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.1.3',
+    version='1.2.0',
 
     description='Moesif Middleware for Python Django',
     long_description=long_description,
