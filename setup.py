53230ebd7c52b8f0d3b82b4be232762d131da9e8
Keyur <keyur@moesif.com>
2019-07-27 17:11:09 -0700

    Refactor: Remove celery from required packages
    Refactor: Remove celery from required packages

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=['requests', 'jsonpickle', 'python-dateutil', 'isodatetimehandler', 'moesifapi',
-                      'moesifpythonrequest', 'apscheduler', 'celery', 'nose'],
+                      'moesifpythonrequest', 'apscheduler', 'nose'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
