de4a2da1d1e35313c6f859ad69b54ec9dffc348e
Keyur <keyur.kv@gmail.com>
2021-03-02 11:42:39 -0800

    Refactor: Prioritize identify user customer hook
    Refactor: Prioritize identify user customer hook
    Bump version to 2.0.2

--- moesifdjango/logger_helper.py
+++ moesifdjango/logger_helper.py
@@ -156,69 +156,69 @@ class LoggerHelper:
     def get_user_id(self, middleware_settings, request, response, request_headers, debug):
         user_id = None
         try:
-            if request.user and request.user.is_authenticated and request.user.username:
-                return request.user.username
-            else:
-                identify_user = middleware_settings.get('IDENTIFY_USER', None)
-                if identify_user is not None:
-                    user_id = identify_user(request, response)
-                if not user_id:
-                    # Transform request headers keys to lower case
-                    request_headers = {k.lower(): v for k, v in request_headers.items()}
-                    # Fetch the auth header name from the config
-                    auth_header_names = middleware_settings.get('AUTHORIZATION_HEADER_NAME', 'authorization').lower()
-                    # Split authorization header name by comma
-                    auth_header_names = [x.strip() for x in auth_header_names.split(',')]
-                    # Fetch the header name available in the request header
-                    token = None
-                    for auth_name in auth_header_names:
-                        # Check if the auth header name in request headers
-                        if auth_name in request_headers:
-                            # Fetch the token from the request headers
-                            token = request_headers[auth_name]
-                            # Split the token by comma
-                            token = [x.strip() for x in token.split(',')]
-                            # Fetch the first available header
-                            if len(token) >= 1:
-                                token = token[0]
-                            else:
-                                token = None
-                            break
-                    # Fetch the field from the config
-                    field = middleware_settings.get('AUTHORIZATION_USER_ID_FIELD', 'sub').lower()
+            identify_user = middleware_settings.get('IDENTIFY_USER', None)
+            if identify_user is not None:
+                user_id = identify_user(request, response)
+            if not user_id:
+                # Transform request headers keys to lower case
+                request_headers = {k.lower(): v for k, v in request_headers.items()}
+                # Fetch the auth header name from the config
+                auth_header_names = middleware_settings.get('AUTHORIZATION_HEADER_NAME', 'authorization').lower()
+                # Split authorization header name by comma
+                auth_header_names = [x.strip() for x in auth_header_names.split(',')]
+                # Fetch the header name available in the request header
+                token = None
+                for auth_name in auth_header_names:
                     # Check if the auth header name in request headers
-                    if token:
-                        # Check if token is of type Bearer
-                        if 'Bearer' in token:
-                            # Fetch the bearer token
-                            token = self.fetch_token(token, 'Bearer')
-                            # Split the bearer token by dot(.)
-                            split_token = self.split_token(token)
-                            # Check if payload is not None
-                            if len(split_token) >= 3 and split_token[1]:
-                                # Parse and set user Id
-                                user_id = self.parse_authorization_header(split_token[1], field, debug)
-                        # Check if token is of type Basic
-                        elif 'Basic' in token:
-                            # Fetch the basic token
-                            token = self.fetch_token(token, 'Basic')
-                            # Decode the token
-                            decoded_token = base64.b64decode(token)
-                            # Transform token to string to be compatible with Python 2 and 3
-                            decoded_token = self.transform_token(decoded_token)
-                            # Fetch and set the user Id
-                            user_id = decoded_token.split(':', 1)[0].strip()
-                        # Check if token is of user-defined custom type
+                    if auth_name in request_headers:
+                        # Fetch the token from the request headers
+                        token = request_headers[auth_name]
+                        # Split the token by comma
+                        token = [x.strip() for x in token.split(',')]
+                        # Fetch the first available header
+                        if len(token) >= 1:
+                            token = token[0]
                         else:
-                            # Split the token by dot(.)
-                            split_token = self.split_token(token)
-                            # Check if payload is not None
-                            if len(split_token) > 1 and split_token[1]:
-                                # Parse and set user Id
-                                user_id = self.parse_authorization_header(split_token[1], field, debug)
-                            else:
-                                # Parse and set user Id
-                                user_id = self.parse_authorization_header(token, field, debug)
+                            token = None
+                        break
+                # Fetch the field from the config
+                field = middleware_settings.get('AUTHORIZATION_USER_ID_FIELD', 'sub').lower()
+                # Check if the auth header name in request headers
+                if token:
+                    # Check if token is of type Bearer
+                    if 'Bearer' in token:
+                        # Fetch the bearer token
+                        token = self.fetch_token(token, 'Bearer')
+                        # Split the bearer token by dot(.)
+                        split_token = self.split_token(token)
+                        # Check if payload is not None
+                        if len(split_token) >= 3 and split_token[1]:
+                            # Parse and set user Id
+                            user_id = self.parse_authorization_header(split_token[1], field, debug)
+                    # Check if token is of type Basic
+                    elif 'Basic' in token:
+                        # Fetch the basic token
+                        token = self.fetch_token(token, 'Basic')
+                        # Decode the token
+                        decoded_token = base64.b64decode(token)
+                        # Transform token to string to be compatible with Python 2 and 3
+                        decoded_token = self.transform_token(decoded_token)
+                        # Fetch and set the user Id
+                        user_id = decoded_token.split(':', 1)[0].strip()
+                    # Check if token is of user-defined custom type
+                    else:
+                        # Split the token by dot(.)
+                        split_token = self.split_token(token)
+                        # Check if payload is not None
+                        if len(split_token) > 1 and split_token[1]:
+                            # Parse and set user Id
+                            user_id = self.parse_authorization_header(split_token[1], field, debug)
+                        else:
+                            # Parse and set user Id
+                            user_id = self.parse_authorization_header(token, field, debug)
+            if not user_id:
+                if request.user and request.user.is_authenticated and request.user.username:
+                    return request.user.username
         except Exception as e:
             if debug:
                 print("can not execute identify_user function, please check moesif settings.")
--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='2.0.1',
+    version='2.0.2',
 
     description='Moesif Middleware for Python Django',
     long_description=long_description,
