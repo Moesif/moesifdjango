6dbd557fa43bd6b0d55ca57b4d12fd51d6da2e22
Keyur <keyur.kv@gmail.com>
2024-10-30 00:42:15 +0000

    #188414493 Refactor: Update moesifapi-python and moesifpythonrequest dependencies  (#88)
    * Bump versions
    
    - Update moesifdjango to 2.3.10
    - Update packages in requirements.txt to allign with moesifapi
    - Update moesifpythonrequest and moesifapi
    
    * Refactor: Update dependencies
    
    Refactor: Update dependencies
    
    ---------
    
    Co-authored-by: Dylan Frankcom <dylanfrankcom@icloud.com>

--- setup.py
+++ setup.py
@@ -28,12 +28,12 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='2.3.9',
+    version='2.3.10',
 
     description='Moesif Middleware for Python Django',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    
+
     # The project's main homepage.
     url='https://www.moesif.com/docs/server-integration/django/',
 
@@ -82,8 +82,8 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['requests', 'jsonpickle', 'python-dateutil', 'isodatetimehandler', 'moesifapi>=1.4.3',
-                      'moesifpythonrequest>=0.3.0', 'apscheduler', 'nose'],
+    install_requires=['requests', 'jsonpickle', 'python-dateutil', 'isodatetimehandler', 'moesifapi>=1.5.3',
+                      'moesifpythonrequest>=0.3.4', 'apscheduler', 'nose'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
