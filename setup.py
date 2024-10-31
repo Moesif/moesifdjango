4eafdd20b75b3cc74b8544a8233572ee6af99a91
Praveen Kumar <praveen@moesif.com>
2024-10-31 13:57:04 -0700

    cleanup

--- setup.py
+++ setup.py
@@ -83,7 +83,8 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=['requests', 'jsonpickle', 'python-dateutil', 'isodatetimehandler', 'moesifapi>=1.5.3',
-                      'moesifpythonrequest>=0.3.4', 'apscheduler', 'nose', 'ipaddress;python_version<"3.3"'],
+                      'moesifpythonrequest>=0.3.4', 'apscheduler', 'nose'
+                      ],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
